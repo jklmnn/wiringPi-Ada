806deaf7edde86d2cb937e8f3568688e90bdf4c7
Johannes Kliemann <jk@jkliemann.de>
2018-08-28 16:09:07 +0200

    add wiringPi specs and example

---
+++ src/wiringpi.ads
@@ -0,0 +1,62 @@
+
+package WiringPi is
+
+    subtype Pin_Type is Integer;
+
+    type Mode_Type is (
+        Input,
+        Output
+        );
+
+    for Mode_Type use (
+        Input => 0,
+        Output => 1
+        );
+
+    type Value_Type is (
+        Low,
+        High
+        );
+
+    for Value_Type use (
+        Low => 0,
+        High => 1
+        );
+
+    function Setup return Integer
+        with
+        Import,
+        Convention => C,
+        External_Name => "wiringPiSetup";
+
+    function Setup_Gpio return Integer
+        with
+        Import,
+        Convention => C,
+        External_Name => "wiringPiSetupGpio";
+
+    function Setup_Phys return Integer
+        with
+        Import,
+        Convention => C,
+        External_Name => "wiringPiSetupPhys";
+
+    procedure Pin_Mode
+        with
+        Import,
+        Convention => C,
+        External_Name => "pinMode";
+
+    procedure Digital_Write (Pin : Pin_Type; Value : Value_Type)
+        with
+        import,
+        Convention => C,
+        External_Name => "digitalWrite";
+
+    function Digital_Read (Pin : Pin_Type) return Value_Type
+        with
+        Import,
+        Convention => C,
+        External_Name => "digitalRead";
+
+end WiringPi;
